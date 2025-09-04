7fd2bcc9fa7f0d71a21eecb730a7c6d370c65ecb
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-09-04 20:13:40 +0300

    Built  Assets

---
+++ lib/src/core/utils/assets.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for appIcon
+  /// assets/app_icon.png
+  static const String appIcon = "assets/app_icon.png";
+
+  /// Assets for fontsRalewayREADME
+  /// assets/fonts/Raleway/README.txt
+  static const String fontsRalewayREADME = "assets/fonts/Raleway/README.txt";
+
+  /// Assets for fontsRalewayRalewayBold
+  /// assets/fonts/Raleway/Raleway-Bold.ttf
+  static const String fontsRalewayRalewayBold = "assets/fonts/Raleway/Raleway-Bold.ttf";
+
+  /// Assets for fontsRalewayRalewayMedium
+  /// assets/fonts/Raleway/Raleway-Medium.ttf
+  static const String fontsRalewayRalewayMedium = "assets/fonts/Raleway/Raleway-Medium.ttf";
+
+  /// Assets for fontsRalewayRalewayRegular
+  /// assets/fonts/Raleway/Raleway-Regular.ttf
+  static const String fontsRalewayRalewayRegular = "assets/fonts/Raleway/Raleway-Regular.ttf";
+
+  /// Assets for fontsRalewayRalewaySemiBold
+  /// assets/fonts/Raleway/Raleway-SemiBold.ttf
+  static const String fontsRalewayRalewaySemiBold = "assets/fonts/Raleway/Raleway-SemiBold.ttf";
+
+  /// Assets for imagesShyError
+  /// assets/images/shy-error.png
+  static const String imagesShyError = "assets/images/shy-error.png";
+
+  /// Assets for imagesSplash
+  /// assets/images/splash.png
+  static const String imagesSplash = "assets/images/splash.png";
+
+  /// Assets for imagesSplashAndroid12
+  /// assets/images/splash_android_12.png
+  static const String imagesSplashAndroid12 = "assets/images/splash_android_12.png";
+}
+
