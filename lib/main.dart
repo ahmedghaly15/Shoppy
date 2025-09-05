db7529f5dfae751fe587a302eaa616f6e9757ffc
ahmedghaly15 <ahmedghaly0767@gmail.com>
2025-09-05 21:12:58 +0300

    Wrapped App with ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart' show ProviderScope;
 import 'package:flutter_screenutil/flutter_screenutil.dart' show ScreenUtil;
 
+import 'riverpod_observer.dart';
 import 'src/core/widgets/flutter_error_details_view.dart';
 import 'src/shoppe_app.dart';
 
@@ -9,5 +11,7 @@ void main() async {
   await ScreenUtil.ensureScreenSize();
   ErrorWidget.builder = (FlutterErrorDetails details) =>
       FlutterErrorDetailsView(details: details);
-  runApp(const ShoppeApp());
+  runApp(
+    ProviderScope(observers: [RiverpodObserver()], child: const ShoppeApp()),
+  );
 }
--- lib/src/shoppe_app.dart
+++ lib/src/shoppe_app.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart' show ScreenUtilInit;
+import 'package:shadcn_ui/shadcn_ui.dart' show ShadApp;
+
+import 'core/utils/app_strings.dart';
 
 class ShoppeApp extends StatelessWidget {
   const ShoppeApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, __) => ShadApp.custom(
+        appBuilder: (context) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: AppStrings.appName,
+          home: Scaffold(
+            body: Center(
+              child: Text(
+                'Welcome',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
